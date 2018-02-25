6b6777258047fe3078323e0b035397e9a6da2473
Rob Taylor <robwt@btinternet.com>
2018-02-25 09:26:44 +0000

    new url, view and template to display lists

--- lists/views.py
+++ lists/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import redirect, render
 from lists.models import Item
 
-# Create your views here.
 def home_page(request):
     if request.method == 'POST':
         Item.objects.create(text=request.POST['item_text'])
-        return redirect('/')
+        return redirect('/lists/the_only_list_in_the_world/')
+    return render(request, 'home.html')
 
+def view_list(request):
     items = Item.objects.all()
-    return render(request, 'home.html', {'items': items})
+    return render(request, 'list.html', {'items': items})
--- superlists/urls.py
+++ superlists/urls.py
@@ -17,5 +17,6 @@ from django.conf.urls import url
 from lists import views
 
 urlpatterns = [
-    url(r'^$', views.home_page, name='home')
+    url(r'^$', views.home_page, name='home'),
+    url(r'^lists/the_only_list_in_the_world/$', views.view_list, name='view_list')
 ]
