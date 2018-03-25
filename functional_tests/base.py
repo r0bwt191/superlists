5361d79c27da4daa4b80eb93fe8c6a2ea3f4365b
Rob Taylor <robwt@btinternet.com>
2018-03-25 11:11:44 +0100

    added manage.py command to allow session creation in staging for FT

--- functional_tests/base.py
+++ functional_tests/base.py
@@ -4,6 +4,8 @@ from selenium.common.exceptions import WebDriverException
 import time
 import os
 
+from.server_tools import reset_database
+
 MAX_WAIT = 10
 
 def wait(fn):
@@ -22,9 +24,10 @@ class FunctionalTest(StaticLiveServerTestCase):
 
     def setUp(self):
         self.browser = webdriver.Firefox()
-        staging_server = os.environ.get('STAGING_SERVER')
-        if staging_server:
-            self.live_server_url = 'http://' + staging_server
+        self.staging_server = os.environ.get('STAGING_SERVER')
+        if self.staging_server:
+            self.live_server_url = 'http://' + self.staging_server
+            reset_database(self.staging_server)
 
     def tearDown(self):
         self.browser.quit()
@@ -53,4 +56,3 @@ class FunctionalTest(StaticLiveServerTestCase):
     @wait
     def wait_for(self, fn):
         return fn()
-        
